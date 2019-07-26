cce9a22f1ff18807a200d8cc20f3628b2d39dc36
root <penadr@sonoma.edu>
2019-07-26 11:45:51 -0700

    Merged changes made to AppNavigator.js

--- UI/navigation/AppNavigator.js
+++ UI/navigation/AppNavigator.js
@@ -4,13 +4,7 @@ import {
 import HomeScreen from '../screens/HomeScreen';
 import ScavengerModeScreen from '../screens/ScavengerModeScreen';
 import SettingScreen from '../screens/SettingScreen';
-<<<<<<< HEAD
-import LanguageSelectScreen from '../screens/LanguageSelectScreen';
-import VolumeScreen from '../screens/VolumeScreen';
-import DiscoveryScreen from '../screens/DiscoveryModeScreen';
-=======
 import DiscoveryModeScreen from '../screens/DiscoveryModeScreen';
->>>>>>> 73abe89fa40e91df9b2932b1f770c4b23a9da08d
 
   const AppNavigator = createStackNavigator(
     {
@@ -26,18 +20,6 @@ import DiscoveryModeScreen from '../screens/DiscoveryModeScreen';
       SettingMode: {
         screen: SettingScreen
       },
-<<<<<<< HEAD
-      DiscoveryMode: {
-        screen: DiscoveryScreen
-      },
-      LanguageSelectMode: {
-        screen: LanguageSelectScreen
-      },
-      VolumeMode: {
-        screen: VolumeScreen
-      },
-=======
->>>>>>> 73abe89fa40e91df9b2932b1f770c4b23a9da08d
     },
     {
       initialRouteName: 'Home',
